ed77e792b873b14973eea96d7018d8ac5b6a4ad5
TrickyVikram <vkumarsah999@gmail.com>
2024-09-29 02:23:23 +0530

    arrayQ

---
+++ Array/ArraySum.java
@@ -0,0 +1,18 @@
+package Array;
+
+// Write a program to find the sum of all elements in an array.
+
+public class ArraySum {
+    public static void main(String[] args) {
+
+        int[] numbers = { 1, 2, 3, 4, 5 };
+
+        int sum = 0;
+
+        for (int num : numbers) {
+            sum += num;
+        }
+
+        System.out.println("Sum of array elements: " + sum);
+    }
+}
